45cd82f13751cba04ff60b7f0ef9ae93bd76eb3e
Fluffybun <Clarence.luna651@gmail.com>
2025-11-01 14:29:20 +0800

    quick fix

--- login.php
+++ login.php
@@ -46,6 +46,8 @@ function userAndPassCorrect(){
 
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login</title>
         <link rel="stylesheet" href="login-signup.css">
     </head>
