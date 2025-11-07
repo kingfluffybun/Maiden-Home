89a914d5b4331f6bdb5d6883cebe28d6ecbcd9b8
harB33 <bautistaharvy@gmail.com>
2025-11-07 09:56:59 +0800

    added search bar

--- includes/nav-bar.php
+++ includes/nav-bar.php
@@ -1,5 +1,14 @@
 <nav>
-    <h1>LOGO</h1>
+    <a href="homepage.php" style="text-decoration: none;">
+        <h1>LOGO</h1>
+    </a>
+    <div class="search">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#484848" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search">
+            <path d="m21 21-4.34-4.34" />
+            <circle cx="11" cy="11" r="8" />
+        </svg>
+        <input name="search" type="search" placeholder="What are you looking for?">
+    </div>
     <div class="nav-options-wrapper">
         <div class="user-dropdown">
             <button type="button" class="nav-options" onclick="toggleUser()">
