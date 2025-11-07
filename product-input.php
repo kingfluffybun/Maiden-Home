c4a6df492c4111ca9bd0d4e84889ed8ccbc415f7
GaZeRnG <gazeringg@gmail.com>
2025-11-07 16:07:33 +0800

    updated

--- product-input.php
+++ product-input.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
-include("./db.php");
+include("includes/db.php");
 
 if (isset($_POST['finish'])) {
 
     $product_name = $_POST['product_name'];
     $price = $_POST['price'];
     $stocks = $_POST['stocks'];
-    $product_img = $_POST['product_img'];
-    $product_overview = $_POST['product_overview'];
     $category_id = $_POST['category_id'];
     $sub_id = $_POST['sub_id'];
     $product_description = $_POST['product_description'];
@@ -33,9 +31,21 @@ if (isset($_POST['finish'])) {
     }
 
     $product_img = uploadFile('product_img');
+    $product_img2 = uploadFile('product_img2');
+    $product_img3 = uploadFile('product_img3');
+    $product_img4 = uploadFile('product_img4');
+    $product_img5 = uploadFile('product_img5');
+    $product_img_hover = uploadFile('product_img_hover');
 
-    $stmt = $conn->prepare("INSERT INTO products (product_name, price, stocks, product_img, product_overview, category_id, sub_id, product_description) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssssss", $product_name, $price, $stocks, $product_img, $product_overview, $category_id, $sub_id, $product_description);
+    $stmt = $conn->prepare("INSERT INTO products (
+            product_name, price, stocks, 
+            product_img, product_img2, product_img3, product_img4, product_img5, 
+            product_img_hover, product_description, category_id, sub_id
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sdisssssssis", 
+        $product_name, $price, $stocks,
+        $product_img, $product_img2, $product_img3, $product_img4, $product_img5,
+        $product_img_hover, $product_description, $category_id, $sub_id);
 
     if ($stmt->execute()) {
         header("Location: product.php");
@@ -56,6 +66,45 @@ if (isset($_POST['finish'])) {
     <meta charset="UTF-8">
     <title>Product Input</title>
     <style>
+        ::-webkit-scrollbar {
+            width: 13px;
+        }
+
+        ::-webkit-scrollbar-track {
+            background: rgb(241 175 123);
+            border-radius: 10px;
+            margin: 0;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            border-radius: 10px;
+            background:  rgb(233, 106, 2);
+            border: 2px solid transparent;
+            background-clip: padding-box;
+            cursor: pointer;
+        }
+
+        ::-webkit-scrollbar-thumb:hover {
+            background: rgb(214, 97, 2);
+            border: 2px solid transparent;
+            background-clip: padding-box;
+        }
+
+        ::-webkit-scrollbar-button:decrement {
+            cursor: pointer;
+            background-size: 20px;
+            height: 15px;
+            border: none;
+            background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='%23d47b33' stroke-width='4.5' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-chevron-up-icon lucide-chevron-up'><path d='m18 15-6-6-6 6'/></svg>") no-repeat center;
+        }
+
+        ::-webkit-scrollbar-button:increment {
+            cursor: pointer;
+            background-size: 20px;
+            height: 15px;
+            border: none;
+            background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="%23d47b33" stroke-width="4.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down-icon lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>') no-repeat center;
+        }
         body {
             font-family: Arial, sans-serif;
             background-color: #f5f5f5;
@@ -70,7 +119,6 @@ if (isset($_POST['finish'])) {
             padding: 20px;
             border-radius: 12px;
             box-shadow: 0 4px 12px rgba(0,0,0,0.1);
-            width: 450px;
         }
 
         h1 {
@@ -81,8 +129,16 @@ if (isset($_POST['finish'])) {
 
         form {
             display: flex;
+            align-items: flex-start;
+            gap: 20px;
+            margin-bottom: 20px;
+        }
+
+        .column {
+            display: flex;
             flex-direction: column;
             gap: 20px;
+            width: 100%;
         }
 
         label {
@@ -112,6 +168,7 @@ if (isset($_POST['finish'])) {
         input[type="submit"] {
             background-color: #d47b33;
             color: white;
+            width: 100%;
             padding: 12px;
             font-size: 1rem;
             border: none;
@@ -129,85 +186,82 @@ if (isset($_POST['finish'])) {
     <div class="form-container">
         <h1>Product Input</h1>
         <form action="" method="post" enctype="multipart/form-data">
-            <label>
-                Product Name
-                <input type="text" name="product_name" placeholder="Product Name" required>
-            </label>
-
-            <label>
-                Price
-                <input type="number" name="price" placeholder="Price" min="0" step="0.01" required>
-            </label>
-
-            <label>
-                Stock
-                <input type="number" name="stocks" placeholder="Stock" min="0" step="1" required>
-            </label>
-
-            <label>
-                Product Image
-                <input type="file" name="product_img" required>
-            </label>
-
-            <label>
-                Product Overview
-                <input type="text" name="product_overview" placeholder="Product Overview" required>
-            </label>
-
-            <label>
-                Product Description
-                <textarea name="product_description" placeholder="Product Description" required></textarea>
-            </label>
-
-            <label>
-                Category
-                <select name="category_id" required>
-                    <option value="" disabled selected>Select Category</option>
-                    <option value="1">Storage & Organization</option>
-                    <option value="2">Beds & Mattresses</option>
-                    <option value="3">Table & Chairs</option>
-                    <option value="4">Sofas & Armchair</option>
-                    <option value="5">Home Decorations</option>
-                    <option value="6">Light Fixtures</option>
-                    <option value="7">Office Furniture</option>
-                    <option value="8">Outdoor Furniture</option>
-                </select>
-            </label>
-
-            <label>
-                Sub-Category
-                <select name="sub_id" required>
-                    <option value="" disabled selected>Select Sub-Category</option>
-                    <option value="1">Bookcases & Shelving Units</option>
-                    <option value="2">Chests of drawers & drawer units</option>
-                    <option value="3">Cabinets & Cupboards</option>
-                    <option value="4">TV & media furniture</option>
-                    <option value="5">Wardrobes & Closet Systems</option>
-                    <option value="6">Beds</option>
-                    <option value="7">Beddings and Pillows</option>
-                    <option value="8">Mattresses</option>
-                    <option value="9">Under bed storage</option>
-                    <option value="10">Headboards</option>
-                    <option value="11">Dining Tables</option>
-                    <option value="12">Dining Chairs & Benches</option>
-                    <option value="13">Coffee & End Tables</option>
-                    <option value="14">Bar & Counter Stools</option>
-                    <option value="15">Specialty Seating</option>
-                    <option value="16">Sofas & Couches</option>
-                    <option value="17">Armchairs</option>
-                    <option value="18">Sofabeds</option>
-                    <option value="19">Ottomans, footstools & pouffes</option>
-                    <option value="20">Coushions</option>
-                    <option value="21">Wall Décor & Mirrors</option>
-                    <option value="22">Vases, Planters & Greenery</option>
-                    <option value="23">Decorative Accents</option>
-                    <option value="24">Rugs & Floor Coverings</option>
-                    <option value="25">Seasonal</option>
-                </select>
-            </label>
-
-            <input type="submit" name="finish" value="Publish">
+            <div class="column">
+                <label>Product Name <input type="text" name="product_name" required></label>
+                <label>Price <input type="number" name="price" required></label>
+                <label>Stock <input type="number" name="stocks" required></label>
+                <label>Product Image <input type="file" name="product_img" required></label>
+                <label>Product Image Hover <input type="file" name="product_img_hover" required></label>
+                <label>Product Angle 1 <input type="file" name="product_img2" required></label>
+            </div>
+            <div class="column">
+                <label>Product Angle 2 <input type="file" name="product_img3" required></label>
+                <label>Product Angle 3 <input type="file" name="product_img4" required></label>
+                <label>Product Angle 4 <input type="file" name="product_img5" required></label>
+                <label>Product Description <textarea name="product_description" placeholder="Product Description" required></textarea></label>
+                <label>
+                    Category
+                    <select name="category_id" required>
+                        <option value="" disabled selected>Select Category</option>
+                        <option value="1">Storage & Organization</option>
+                        <option value="2">Beds & Mattresses</option>
+                        <option value="3">Table & Chairs</option>
+                        <option value="4">Sofas & Armchair</option>
+                        <option value="5">Home Decorations</option>
+                        <option value="6">Light Fixtures</option>
+                        <option value="7">Office Furniture</option>
+                        <option value="8">Outdoor Furniture</option>
+                    </select>
+                </label>
+                <label>
+                    Sub-Category
+                    <select name="sub_id" required>
+                        <option value="" disabled selected>Select Sub-Category</option>
+                        <option value="1">Bookcases & Shelving Units</option>
+                        <option value="2">Chests of drawers & drawer units</option>
+                        <option value="3">Cabinets & Cupboards</option>
+                        <option value="4">TV & media furniture</option>
+                        <option value="5">Wardrobes & Closet Systems</option>
+                        <option value="6">Beds</option>
+                        <option value="7">Beddings and Pillows</option>
+                        <option value="8">Mattresses</option>
+                        <option value="9">Under bed storage</option>
+                        <option value="10">Headboards</option>
+                        <option value="11">Dining Tables</option>
+                        <option value="12">Dining Chairs & Benches</option>
+                        <option value="13">Coffee & End Tables</option>
+                        <option value="14">Bar & Counter Stools</option>
+                        <option value="15">Specialty Seating</option>
+                        <option value="16">Sofas & Couches</option>
+                        <option value="17">Armchairs</option>
+                        <option value="18">Sofabeds</option>
+                        <option value="19">Ottomans, footstools & pouffes</option>
+                        <option value="20">Coushions</option>
+                        <option value="21">Wall Décor & Mirrors</option>
+                        <option value="22">Vases, Planters & Greenery</option>
+                        <option value="23">Decorative Accents</option>
+                        <option value="24">Rugs & Floor Coverings</option>
+                        <option value="25">Seasonal</option>
+                        <option value="26">Ceiling Lights & Pendants</option>
+                        <option value="27">Floor Lamps</option>
+                        <option value="28">Table & Desk Lamps</option>
+                        <option value="29">Wall & Vanity Lights</option>
+                        <option value="30">Outdoor Lighting</option>
+                        <option value="31">Desks & Work Surfaces</option>
+                        <option value="32">Office & Task Chairs</option>
+                        <option value="33">Home Office Sets</option>
+                        <option value="34">Gaming Furniture</option>
+                        <option value="35">Filing & Office Storage</option>
+                        <option value="36">Outdoor Lounge & Seating</option>
+                        <option value="37">Outdoor Dining & Bar Sets</option>
+                        <option value="38">Umbrellas, Pergolas & Shade</option>
+                        <option value="39">Outdoor Storage</option>
+                        <option value="40">Outdoor Décor & Accents</option>
+                    </select>
+                </label>
+            </div>
         </form>
+        <input type="submit" name="finish" value="Publish">
     </div>
 </body>
 </html>
--- product.php
+++ product.php
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-<?php
+<?php 
 session_start();
-include("./db.php");
+include("includes/db.php");
 
     if (!isset($_SESSION['username']) && isset($_COOKIE['username'])) {
         $_SESSION['username'] = $_COOKIE['username'];
@@ -12,44 +11,16 @@ include("./db.php");
         FROM products p
         JOIN category c ON p.category_id = c.category_id";
 
-    if (isset($_GET['sort']) && $_GET['sort'] == 'high') {
-        $sql = "SELECT * FROM products ORDER BY price DESC";
-    } else {
-        $sql = "SELECT * FROM products";
-    }
-
     $result = mysqli_query($conn, $sql);
 
 ?>
 
 <html>
-<head>
-    <title>Products | Maiden Home</title>
-    <link rel="stylesheet" href="product.css">
-</head>
-<body>
-    <nav>
-        <h1>LOGO</h1>
-        <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
-            <a href="./product-input.php" style="text-decoration: none; color: white;">Input Products Here</a>
-        <?php endif; ?>
-        <div class="nav-options">
-            <a href="login.php"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.6"><circle cx="12" cy="12" r="10"/><circle cx="12" cy="10" r="3"/><path d="M7 20v-1a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1"/></svg></a>
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.6"><path d="M2 9.5a5.5 5.5 0 0 1 9.6-3.7.6.6 0 0 0 .8 0A5.5 5.5 0 0 1 22 9.5c0 2.3-1.5 4-3 5.5l-5.5 5.3a2 2 0 0 1-3 0L5 15C3.5 13.5 2 11.8 2 9.5"/></svg></a>
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.6"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2 2h2l2.6 12.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 1.9-1.6l1.6-7.4H5.1"/></svg></a>
-        </div>
-    </nav>
-
-    <section class="category-banner">
-        <img src="assets/PRODUCTS/BANNER.jpg" alt="Banner">
-    </section>
-
-=======
-<html>
 
 <head>
     <title>Products | Maiden Home</title>
     <link rel="stylesheet" href="css/product.css">
+    <link rel="stylesheet" href="css/scroll.css">
     <link rel="stylesheet" href="css/nav-bar.css">
     <script src="js/script.js"></script>
 </head>
@@ -59,7 +30,6 @@ include("./db.php");
     <section class="category-banner">
         <img src="assets/PRODUCTS/BANNER.jpg" alt="Banner">
     </section>
->>>>>>> main
     <section class="sort-bar">
         <div class="sort-area">
             <p>Sort By:</p>
@@ -67,8 +37,10 @@ include("./db.php");
                 <button class="active">Featured</button>
                 <button>Latest</button>
                 <button>Best Selling</button>
-<<<<<<< HEAD
                 <a href="product.php?sort=high"><button>Price: High to Low</button></a>
+                <?php if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin'): ?>
+                    <a href="./product-input.php"><button>Add Product</button></a>
+                <?php endif; ?>
             </div>
         </div>
     </section>
@@ -84,200 +56,6 @@ include("./db.php");
             <p>₱<?= $row['price']; ?></p>
         </div>
         <?php endwhile; ?>
-=======
-                <button>Price: High to Low</button>
-            </div>
-        </div>
-    </section>
-    <section class="product-grid">
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 1</h3>
-            <p>₱2,999</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 2</h3>
-            <p>₱3,299</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 3</h3>
-            <p>₱4,150</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 4</h3>
-            <p>₱3,450</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 5</h3>
-            <p>₱2,999</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 6</h3>
-            <p>₱3,199</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 7</h3>
-            <p>₱5,399</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 8</h3>
-            <p>₱4,899</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 9</h3>
-            <p>₱6,000</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 10</h3>
-            <p>₱3,450</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 11</h3>
-            <p>₱3,850</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 12</h3>
-            <p>₱2,850</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 13</h3>
-            <p>₱2,999</p>
-        </div>
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 14</h3>
-            <p>₱6,149</p>
-        </div>
-
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 15</h3>
-            <p>₱3,250</p>
-        </div>
-
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 16</h3>
-            <p>₱5,750</p>
-        </div>
-
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 17</h3>
-            <p>₱4,650</p>
-        </div>
-
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 18</h3>
-            <p>₱3,999</p>
-        </div>
-
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 19</h3>
-            <p>₱2,999</p>
-        </div>
-
-        <div class="product-card">
-            <div class="img-container">
-                <img src="assets/PRODUCTS/CHAIR.jpg" class="default-img">
-                <img src="assets/PRODUCTS/CHAIR2.jpg" class="hover-img">
-                <button class="add-btn"><img src="assets/PRODUCTS/ADDICON.png"></button>
-            </div>
-            <h3>Chair 20</h3>
-            <p>₱6,499</p>
-        </div>
-
->>>>>>> main
     </section>
 
     <section class="pagination">
@@ -292,9 +70,4 @@ include("./db.php");
         <button class="next">&gt;</button>
     </section>
 </body>
-<<<<<<< HEAD
 </html>
-=======
-
-</html>
->>>>>>> main
