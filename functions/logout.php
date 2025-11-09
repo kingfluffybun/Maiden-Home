dad92602cc811374b2673f8b383e47ff695264c1
GaZeRnG <gazeringg@gmail.com>
2025-11-09 15:45:47 +0800

    fixed

--- functions/logout.php
+++ functions/logout.php
@@ -8,5 +8,5 @@ session_destroy();
 setcookie("username", "", time() - 3600, "/");
 setcookie("user_email", "", time() - 3600, "/");
 
-header("Location: ../login.php");
+header("Location: /Maiden-Home/login/");
 exit;
