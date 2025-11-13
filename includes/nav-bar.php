941d8093f70fc09e5254d3ded6dcaed84e320656
Jhon Rick <henry.kashlie@gmail.com>
2025-11-14 00:02:03 +0800

    fixed nav bar

--- includes/nav-bar.php
+++ includes/nav-bar.php
@@ -1,12 +1,9 @@
-<?php
-$productPage = '/maiden-home/product/index.php';
-?>
 <nav>
-    <a href="../" style="text-decoration: none;" id="home-link"><div class="logo">
-        <img src="../assets/Logo.png" alt="">
+    <a href="/Maiden-Home/" style="text-decoration: none;" id="home-link"><div class="logo">
+        <img src="/Maiden-Home/assets/Logo.png" alt="">
         <h1 style="color: #d47b33;">MAIDEN HOME</h1>
     </div></a>
-        <form action="<?= $productPage ?>" method="GET" class="search">
+        <form action="/maiden-home/product/index.php" method="GET" class="search">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#484848" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search">
                 <path d="m21 21-4.34-4.34" />
                 <circle cx="11" cy="11" r="8" />
