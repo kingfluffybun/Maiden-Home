dc517e12d5fb8ccf3bed2b224e3c89151ea0ebb3
Fluffybun <Clarence.luna651@gmail.com>
2025-11-23 23:23:42 +0800

    added admin dashboard

--- includes/nav-bar.php
+++ includes/nav-bar.php
@@ -61,6 +61,16 @@
                                 </div>
                             </a>
                         </li>
+                        <?php if (isset($_SESSION['role']) == "admin"): ?>
+                        <li>
+                            <a href="/Maiden-Home/admin">
+                                <div style="display: flex; align-items:center; gap:8px;">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-cog-icon lucide-user-round-cog"><path d="m14.305 19.53.923-.382"/><path d="m15.228 16.852-.923-.383"/><path d="m16.852 15.228-.383-.923"/><path d="m16.852 20.772-.383.924"/><path d="m19.148 15.228.383-.923"/><path d="m19.53 21.696-.382-.924"/><path d="M2 21a8 8 0 0 1 10.434-7.62"/><path d="m20.772 16.852.924-.383"/><path d="m20.772 19.148.924.383"/><circle cx="10" cy="8" r="5"/><circle cx="18" cy="18" r="3"/></svg>
+                                    <p>Admin Dashboard</p>
+                                </div>
+                            </a>
+                        </li>
+                        <?php endif; ?>
                         <li>
                             <a href="/Maiden-Home/functions/logout.php">
                                 <div style="display: flex; align-items:center; gap:8px;">
