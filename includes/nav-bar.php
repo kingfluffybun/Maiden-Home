40747bfa0c8a804adb2a26bc07e5f964d96b881f
Fluffybun <Clarence.luna651@gmail.com>
2025-11-26 06:11:03 +0800

    preparing cart item count function

--- includes/nav-bar.php
+++ includes/nav-bar.php
@@ -1,3 +1,21 @@
+<?php /*
+$user_id = $_SESSION['user_id'];
+
+$sql = "SELECT COUNT(cart_id) AS item_count
+        FROM addtocart
+        WHERE user_id = ?";
+        
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+// Fetch the result
+$row = $result->fetch_assoc();
+
+// The total count of items in the cart
+$cart_item_count = $row['item_count']; */
+?>
 <nav>
     <a href="/Maiden-Home/" style="text-decoration: none;" id="home-link"><div class="logo">
         <img src="/Maiden-Home/assets/Logo.png" alt="">
@@ -91,13 +109,12 @@
                 <?php endif; ?>
             </div>
         </div>
-        <a class="nav-options"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-icon lucide-heart">
-                <path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" />
-            </svg></a>
-        <a href="/Maiden-Home/cart" class="nav-options"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart-icon lucide-shopping-cart">
-                <circle cx="8" cy="21" r="1" />
-                <circle cx="19" cy="21" r="1" />
-                <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
-            </svg></a>
+        <a class="nav-options">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-icon lucide-heart"> <path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" /></svg>
+        </a>
+        <a href="/Maiden-Home/cart" class="nav-options">
+            <?php // echo $cart_item_count; ?>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart-icon lucide-shopping-cart"> <circle cx="8" cy="21" r="1" /> <circle cx="19" cy="21" r="1" /> <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" /> </svg>
+        </a>
     </div>
 </nav>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+    // include "includes/db.php";
     if (!isset($_SESSION['username']) && isset($_COOKIE['username'])) {
         $_SESSION['user_id'] = $_COOKIE['user_id'];
         $_SESSION['username'] = $_COOKIE['username'];
